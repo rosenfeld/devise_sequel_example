920c0842e8f526f4bf7bc56ca1526d878da24c3c
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2013-10-07 14:31:13 -0300

    Don't authenticate for Devise controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_user!
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    authenticate_user! unless params[:controller] == 'users'
+  end
 end
