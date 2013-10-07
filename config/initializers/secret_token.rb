c5323ce5a6bd441bb9d01906e61689b9547a2b15
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2013-10-07 14:11:28 -0300

    rails new devise_example_sequel -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseExampleSequel::Application.config.secret_key_base = '035cde0dd902062b9ba4b107078035ea643f008b93c512a7a73eb662ddfaebb0d8d2eb92ab2fa0d48baa11188873f5c4061222b95729f49a2ec8a72bceb7d1ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeviseExampleSequel::Application.config.session_store :cookie_store, key: '_devise_example_sequel_session'
